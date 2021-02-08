e7cabc95e7a8d401923402c41955d076ff921a8b
twisterry <53815508+twisterry@users.noreply.github.com>
2021-02-08 10:44:50 +0100

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,77 @@
+﻿
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim appdata As String
+        appdata = Environ$("appdata")
+        If ComboBox1.Text = "Game Server" Then
+            My.Computer.FileSystem.CopyFile(
+    "game_servers.dat",
+    appdata + "\.minecraft\servers.dat", overwrite:=True)
+            MsgBox("Erfolgreich!")
+        End If
+        If ComboBox1.Text = "Coole Server :D" Then
+            My.Computer.FileSystem.CopyFile(
+    "cool_servers.dat",
+    appdata + "\.minecraft\servers.dat", overwrite:=True)
+            MsgBox("Erfolgreich!")
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WebBrowser1.Navigate("http://ponolisoft.epizy.com/mcst.php?lang=de")
+        Button3.Enabled = False
+        Me.Text = "MCSLT v0.1 R2"
+        If My.Computer.FileSystem.FileExists("game_servers.dat") Then
+        Else
+            My.Computer.Network.DownloadFile(
+    "http://benheymann.de/7pWy8Hf4eaqd25qpnSSrBWnZGL5XDWe6dbJYSEp/files/server_lists/game_servers.dat",
+    "game_servers.dat", False, 500)
+        End If
+
+        If My.Computer.FileSystem.FileExists("cool_servers.dat") Then
+        Else
+            My.Computer.Network.DownloadFile(
+    "http://benheymann.de/7pWy8Hf4eaqd25qpnSSrBWnZGL5XDWe6dbJYSEp/files/server_lists/cool_servers.dat",
+    "cool_servers.dat", False, 500)
+        End If
+    End Sub
+
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        My.Computer.FileSystem.DeleteFile("game_servers.dat")
+        My.Computer.FileSystem.DeleteFile("cool_servers.dat")
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim appdata As String
+        appdata = Environ$("appdata")
+        If My.Computer.FileSystem.FileExists(appdata + "\.minecraft\servers.dat") Then
+            My.Computer.FileSystem.DeleteFile(appdata + "\.minecraft\servers.dat")
+            MsgBox("Erfolgreich!")
+        Else
+            MsgBox("Es sind keine Server vorhanden!")
+        End If
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Button3.Enabled = True
+        Button3.Text = "German"
+        Button4.Text = "English"
+        Button4.Enabled = False
+        Label1.Text = "Select a List:"
+        Button1.Text = "Save"
+        Button2.Text = "Delete all Servers"
+        WebBrowser1.Navigate("http://ponolisoft.epizy.com/mcst.php?lang=en")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Button3.Enabled = False
+        Button3.Text = "Deutsch"
+        Button4.Text = "Englisch"
+        Button4.Enabled = True
+        Label1.Text = "Bitte eine Liste wählen:"
+        Button1.Text = "Speichern"
+        Button2.Text = "Alle Server löschen!"
+        WebBrowser1.Navigate("http://ponolisoft.epizy.com/mcst.php?lang=de")
+    End Sub
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Allgemeine Informationen über eine Assembly werden über die folgenden
+' Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+' die einer Assembly zugeordnet sind.
+
+' Werte der Assemblyattribute überprüfen
+
+<Assembly: AssemblyTitle("Minecraft Server List Tool R2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Minecraft Server List Tool R2")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Die folgende GUID wird für die typelib-ID verwendet, wenn dieses Projekt für COM verfügbar gemacht wird.
+<Assembly: Guid("7d22e79b-a7ea-480b-b6dc-1d6519f28b3e")>
+
+' Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+'
+'      Hauptversion
+'      Nebenversion
+'      Buildnummer
+'      Revision
+'
+' Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+' indem Sie "*" wie unten gezeigt eingeben:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
