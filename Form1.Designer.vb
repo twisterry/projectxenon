446c174170a99de5ecf99b9294808eba302c63c6
twisterry <benheymann8@gmail.com>
2021-03-16 18:31:47 +0100

    versch. Aktualisierungen

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -36,6 +36,8 @@ Partial Class Form1
         Me.WebBrowser2 = New System.Windows.Forms.WebBrowser()
         Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
         Me.Button5 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button6 = New System.Windows.Forms.Button()
         Me.TabControl1.SuspendLayout()
         Me.TabPage2.SuspendLayout()
         Me.TabPage1.SuspendLayout()
@@ -44,17 +46,17 @@ Partial Class Form1
         'ComboBox1
         '
         Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"Game Server", "Cool Servers"})
-        Me.ComboBox1.Location = New System.Drawing.Point(9, 53)
+        Me.ComboBox1.Items.AddRange(New Object() {"Game Server", "Partner Server", "eingesendete Server"})
+        Me.ComboBox1.Location = New System.Drawing.Point(9, 51)
         Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(218, 21)
+        Me.ComboBox1.Size = New System.Drawing.Size(270, 21)
         Me.ComboBox1.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Tempus Sans ITC", 11.25!, System.Drawing.FontStyle.Bold)
-        Me.Label1.Location = New System.Drawing.Point(12, 31)
+        Me.Label1.Location = New System.Drawing.Point(8, 29)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(178, 19)
         Me.Label1.TabIndex = 1
@@ -62,9 +64,9 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(237, 53)
+        Me.Button1.Location = New System.Drawing.Point(285, 50)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(64, 20)
+        Me.Button1.Size = New System.Drawing.Size(64, 21)
         Me.Button1.TabIndex = 2
         Me.Button1.Text = "Speichern"
         Me.Button1.UseVisualStyleBackColor = True
@@ -73,27 +75,29 @@ Partial Class Form1
         '
         Me.Button2.BackColor = System.Drawing.Color.DarkRed
         Me.Button2.ForeColor = System.Drawing.SystemColors.Control
-        Me.Button2.Location = New System.Drawing.Point(9, 341)
+        Me.Button2.Location = New System.Drawing.Point(8, 340)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(292, 39)
+        Me.Button2.Size = New System.Drawing.Size(341, 36)
         Me.Button2.TabIndex = 3
         Me.Button2.Text = "Alle Server löschen!"
         Me.Button2.UseVisualStyleBackColor = False
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(163, 6)
+        Me.Button4.Location = New System.Drawing.Point(201, 5)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(64, 21)
+        Me.Button4.Size = New System.Drawing.Size(148, 22)
         Me.Button4.TabIndex = 6
         Me.Button4.Text = "Englisch"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(80, 6)
+        Me.Button3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button3.Location = New System.Drawing.Point(8, 5)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(64, 22)
+        Me.Button3.Size = New System.Drawing.Size(144, 22)
         Me.Button3.TabIndex = 5
         Me.Button3.Text = "Deutsch"
         Me.Button3.UseVisualStyleBackColor = True
@@ -102,10 +106,10 @@ Partial Class Form1
         '
         Me.TabControl1.Controls.Add(Me.TabPage2)
         Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Location = New System.Drawing.Point(9, 112)
+        Me.TabControl1.Location = New System.Drawing.Point(9, 108)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(292, 230)
+        Me.TabControl1.Size = New System.Drawing.Size(340, 230)
         Me.TabControl1.TabIndex = 7
         '
         'TabPage2
@@ -114,7 +118,7 @@ Partial Class Form1
         Me.TabPage2.Location = New System.Drawing.Point(4, 22)
         Me.TabPage2.Name = "TabPage2"
         Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(284, 204)
+        Me.TabPage2.Size = New System.Drawing.Size(332, 204)
         Me.TabPage2.TabIndex = 1
         Me.TabPage2.Text = "TabPage2"
         Me.TabPage2.UseVisualStyleBackColor = True
@@ -125,7 +129,7 @@ Partial Class Form1
         Me.WebBrowser1.Location = New System.Drawing.Point(3, 3)
         Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
         Me.WebBrowser1.Name = "WebBrowser1"
-        Me.WebBrowser1.Size = New System.Drawing.Size(278, 198)
+        Me.WebBrowser1.Size = New System.Drawing.Size(326, 198)
         Me.WebBrowser1.TabIndex = 0
         '
         'TabPage1
@@ -150,26 +154,50 @@ Partial Class Form1
         '
         'ProgressBar1
         '
-        Me.ProgressBar1.Location = New System.Drawing.Point(9, 80)
+        Me.ProgressBar1.Location = New System.Drawing.Point(9, 78)
         Me.ProgressBar1.Name = "ProgressBar1"
-        Me.ProgressBar1.Size = New System.Drawing.Size(292, 23)
+        Me.ProgressBar1.Size = New System.Drawing.Size(340, 23)
         Me.ProgressBar1.TabIndex = 8
         '
         'Button5
         '
-        Me.Button5.Location = New System.Drawing.Point(184, 109)
+        Me.Button5.Location = New System.Drawing.Point(231, 105)
         Me.Button5.Name = "Button5"
         Me.Button5.Size = New System.Drawing.Size(117, 20)
         Me.Button5.TabIndex = 9
         Me.Button5.Text = "Listen aktualisieren"
         Me.Button5.UseVisualStyleBackColor = True
         '
+        'Label2
+        '
+        Me.Label2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(5, 404)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Label2"
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(278, 396)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(64, 21)
+        Me.Button6.TabIndex = 11
+        Me.Button6.Text = "Beenden"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.SystemColors.ControlLight
-        Me.ClientSize = New System.Drawing.Size(313, 388)
+        Me.BackColor = System.Drawing.Color.Silver
+        Me.ClientSize = New System.Drawing.Size(361, 426)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button5)
         Me.Controls.Add(Me.ProgressBar1)
         Me.Controls.Add(Me.TabControl1)
@@ -181,7 +209,6 @@ Partial Class Form1
         Me.Controls.Add(Me.ComboBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.TabControl1.ResumeLayout(False)
@@ -205,4 +232,6 @@ Partial Class Form1
     Friend WithEvents WebBrowser2 As WebBrowser
     Friend WithEvents ProgressBar1 As ProgressBar
     Friend WithEvents Button5 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button6 As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -4,13 +4,16 @@ Imports System.Text
 
 
 Public Class Form1
+    Dim startzeit As New TimeSpan(Now.Day, Now.Month, Now.Hour, Now.Minute)
+    Dim version As String = "v2021.3.16"
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         If ComboBox1.Text = "" Then
             If Button3.Text = "German" Then
-                MsgBox("please select a List:")
+                MsgBox("please select a List! (error 0103)")
             Else
-                MsgBox("Bitte wähle eine Liste!")
+                MsgBox("Bitte wähle eine Liste! (Fehler 0103)")
             End If
+            My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " error while applying list (code: 0103)!" & vbCrLf, True)
         Else
             If ComboBox1.Text = "56633" Then
                 If Button3.Text = "German" Then
@@ -18,13 +21,19 @@ Public Class Form1
                 Else
                     MsgBox("DU HAST EIN EASTEER EGG GEFUNDEN!!!!!")
                 End If
+                My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " Easter Egg found!" & vbCrLf, True)
             Else
+                If ComboBox1.Text = "56634" Then
+                    MsgBox("O̴̡̧̢͎̖̘̹̪̜̟̞̼̙̭͈̟͚̩̺̞̱̫̳͉̭͖̦͚͈̦̒͌͛̓̌͒̊̇͒̆̉̎͗̓̀̔͑́̀͑́̀̓̚͝͠͠͝ͅͅM̵̢̧̥̻̬̘͍̻͍̬̜͔̣͈̟̝̤̯̹̹̖͕͚͓̯̬̱̯̤͔̲͈̮̝͉̞̫̖̦͓̪̦̻͑́̍̾̓̈́̑͝Ģ̷̧̧̡̛̮̩͇͎͚̝̠̜̣̹͚̰̝̹͓̺͎̙̼̫̙̜̬͈̩̃̓͊̍̋͆̔̉̆̈́̒̂̇͋́̈́̏̎͆̈́̈́̆̌̍̌͆͊̅́͆͘̚͘̚͜͠͝ͅ")
+                    My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " Easter Egg found!" & vbCrLf, True)
+                End If
             End If
         End If
 
+
         Dim appdata As String
         appdata = Environ$("appdata")
-        If ComboBox1.Text = "Game Server" Then
+        If (ComboBox1.Text = "Game Server") Or (ComboBox1.Text = "game servers") Then
             My.Computer.FileSystem.CopyFile(
     "game_servers.dat",
     appdata + "\.minecraft\servers.dat", overwrite:=True)
@@ -33,8 +42,9 @@ Public Class Form1
             Else
                 MsgBox("Erfolgreich!")
             End If
+            My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " applyed Game_Servers!" & vbCrLf, True)
         End If
-        If ComboBox1.Text = "Cool Servers" Then
+        If (ComboBox1.Text = "Partner Server") Or (ComboBox1.Text = "partner servers") Then
             My.Computer.FileSystem.CopyFile(
     "cool_servers.dat",
     appdata + "\.minecraft\servers.dat", overwrite:=True)
@@ -43,43 +53,85 @@ Public Class Form1
             Else
                 MsgBox("Erfolgreich!")
             End If
+            My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " applyed Cool_Server!" & vbCrLf, True)
+        End If
+        If (ComboBox1.Text = "eingesendete Server") Or (ComboBox1.Text = "submitted servers") Then
+            My.Computer.FileSystem.CopyFile(
+        "sub_servers.dat",
+        appdata + "\.minecraft\servers.dat", overwrite:=True)
+            If Button3.Text = "German" Then
+                    MsgBox("successfully!")
+                Else
+                    MsgBox("Erfolgreich!")
+                End If
+            My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " applyed submitted_servers!" & vbCrLf, True)
         End If
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ProgressBar1.Maximum = "100"
-        ProgressBar1.Minimum = "0"
-        ProgressBar1.Value = "0"
-        ProgressBar1.Value = "10"
-        TabPage2.Text = "Neuigkeiten"
-        TabPage1.Text = "Updates"
-        WebBrowser1.Navigate("http://ponolisoft.epizy.com/mcst.php?lang=de")
-        ProgressBar1.Value = "20"
-        WebBrowser2.Navigate("http://ponolisoft.epizy.com/mcslt_updates")
-        ProgressBar1.Value = "30"
-        Button3.Enabled = False
-        Me.Text = "MCSLT v0.3 R2"
-        ProgressBar1.Value = "40"
-        If My.Computer.FileSystem.FileExists("game_servers.dat") Then
+        My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " Program started!" & vbCrLf, True)
+        If Not My.Computer.Network.IsAvailable Then
+            MsgBox("Bitte überprüfe deine Internet verbindung!")
+            Me.Close()
         Else
-            My.Computer.Network.DownloadFile(
-    "http://benheymann.de/7pWy8Hf4eaqd25qpnSSrBWnZGL5XDWe6dbJYSEp/files/server_lists/game_servers.dat",
-    "game_servers.dat", False, 500)
+            ProgressBar1.Maximum = "100"
+            ProgressBar1.Minimum = "0"
+            ProgressBar1.Value = "0"
+            ProgressBar1.Value = "10"
+            TabPage2.Text = "Neuigkeiten"
+            TabPage1.Text = "Updates"
+            WebBrowser1.Navigate("http://ponolisoft.epizy.com/mcst.php?lang=de")
+            ProgressBar1.Value = "20"
+            WebBrowser2.Navigate("http://ponolisoft.epizy.com/mcslt_updates")
+            My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " Websites loaded!" & vbCrLf, True)
+            ProgressBar1.Value = "30"
+            Button3.Enabled = False
+            Label2.Text = "MCSLT " + version + " by Twisterry"
+            Me.Text = "MCSLT " + version
+            ProgressBar1.Value = "40"
+            If My.Computer.FileSystem.FileExists("game_servers.dat") Then
+            Else
+                If My.Computer.Network.IsAvailable Then
+                    My.Computer.Network.DownloadFile(
+        "https://raw.githubusercontent.com/twisterry/mcserverlisttool/main/listen/game_servers.dat",
+        "game_servers.dat", False, 500)
+                Else
+                    MsgBox("Fehler: 0101")
+                End If
+                ProgressBar1.Value = "50"
+                If My.Computer.FileSystem.FileExists("cool_servers.dat") Then
+                Else
+                    ProgressBar1.Value = "70"
+                    If My.Computer.Network.IsAvailable Then
+                        My.Computer.Network.DownloadFile(
+        "https://raw.githubusercontent.com/twisterry/mcserverlisttool/main/listen/cool_servers.dat",
+        "cool_servers.dat", False, 500)
+                    Else
+                        MsgBox("Fehler 0101")
+                    End If
+                End If
+
+                If My.Computer.Network.IsAvailable Then
+                    My.Computer.Network.DownloadFile(
+    "https://raw.githubusercontent.com/twisterry/mcserverlisttool/main/listen/sub_servers.dat",
+    "sub_servers.dat", False, 500)
+                Else
+                    MsgBox("Fehler 0101")
+                End If
+            End If
+
+            ProgressBar1.Value = "0"
+            My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " All files ready!" & vbCrLf, True)
         End If
-        ProgressBar1.Value = "50"
-        If My.Computer.FileSystem.FileExists("cool_servers.dat") Then
-        Else
-            ProgressBar1.Value = "70"
-            My.Computer.Network.DownloadFile(
-    "http://benheymann.de/7pWy8Hf4eaqd25qpnSSrBWnZGL5XDWe6dbJYSEp/files/server_lists/cool_servers.dat",
-    "cool_servers.dat", False, 500)
-        End If
-        ProgressBar1.Value = "0"
+
     End Sub
 
     Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
         My.Computer.FileSystem.DeleteFile("game_servers.dat")
         My.Computer.FileSystem.DeleteFile("cool_servers.dat")
+        My.Computer.FileSystem.DeleteFile("sub_servers.dat")
+        My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " Program closed!" & vbCrLf, True)
+        My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "-----------" & vbCrLf, True)
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
@@ -92,12 +144,14 @@ Public Class Form1
             Else
                 MsgBox("Erfolgreich!")
             End If
+            My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " all servers deleted successfully!" & vbCrLf, True)
         Else
             If Button3.Text = "German" Then
-                MsgBox("There are no servers!")
+                MsgBox("There are no servers! (error: 0102)")
             Else
-                MsgBox("Es sind keine Server vorhanden!")
+                MsgBox("Es sind keine Server vorhanden! (Fehler: 0102)")
             End If
+            My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " error while deleting servers (code: 0102)" & vbCrLf, True)
 
         End If
 
@@ -113,6 +167,12 @@ Public Class Form1
         Button1.Text = "save"
         Button2.Text = "delete all Servers"
         Button5.Text = "reload lists"
+        Button6.Text = "close"
+        ComboBox1.DataSource = Nothing
+        ComboBox1.Items.Clear()
+        ComboBox1.Items.Add("game servers")
+        ComboBox1.Items.Add("partner servers")
+        ComboBox1.Items.Add("submitted servers")
         WebBrowser1.Navigate("http://ponolisoft.epizy.com/mcst.php?lang=en")
     End Sub
 
@@ -126,29 +186,43 @@ Public Class Form1
         Button1.Text = "Speichern"
         Button2.Text = "Alle Server löschen!"
         Button5.Text = "Listen aktualisieren"
+        Button6.Text = "Beenden"
+        ComboBox1.DataSource = Nothing
+        ComboBox1.Items.Clear()
+        ComboBox1.Items.Add("Game Server")
+        ComboBox1.Items.Add("Partner Server")
+        ComboBox1.Items.Add("eingesendete Server")
         WebBrowser1.Navigate("http://ponolisoft.epizy.com/mcst.php?lang=de")
     End Sub
 
-    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
-        My.Computer.FileSystem.DeleteFile("game_servers.dat")
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click 'neuladen der Listen
+        My.Computer.FileSystem.DeleteFile("game_servers.dat") 'alte Listen löschen
         My.Computer.FileSystem.DeleteFile("cool_servers.dat")
-        ProgressBar1.Maximum = "100"
+        ProgressBar1.Maximum = "100" 'ProgressBar einstellen
         ProgressBar1.Minimum = "0"
-        ProgressBar1.Value = "0"
-        My.Computer.Network.DownloadFile(
-    "http://benheymann.de/7pWy8Hf4eaqd25qpnSSrBWnZGL5XDWe6dbJYSEp/files/server_lists/cool_servers.dat",
+        ProgressBar1.Value = "0" 'Progressbar ist bei 0
+        My.Computer.Network.DownloadFile( 'neue Listen werden Heruntergeladen
+    "https://raw.githubusercontent.com/twisterry/mcserverlisttool/main/listen/cool_servers.dat",
     "cool_servers.dat", False, 500)
         ProgressBar1.Value = "50"
         My.Computer.Network.DownloadFile(
-    "http://benheymann.de/7pWy8Hf4eaqd25qpnSSrBWnZGL5XDWe6dbJYSEp/files/server_lists/game_servers.dat",
+    "https://raw.githubusercontent.com/twisterry/mcserverlisttool/main/listen/game_servers.dat",
     "game_servers.dat", False, 500)
         ProgressBar1.Value = "100"
-        If Button3.Text = "German" Then
+        If Button3.Text = "German" Then 'erfolgsmeldung
             MsgBox("successfully!")
         Else
             MsgBox("Erfolgreich!")
         End If
+        My.Computer.FileSystem.WriteAllText("C://mcslt_log.txt", "[" + startzeit.ToString + "]" + " successfully reloaded lists!" & vbCrLf, True)
         ProgressBar1.Value = "0"
     End Sub
 
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Me.Close()
+    End Sub
 End Class
